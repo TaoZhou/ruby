5dc775cc1edefeb5ce7e15e638a47223e09ccf6f
zhou tao <zhou.tao@integrasco.no>
2013-11-02 22:54:32 +0800

    Implement the CRU in "CRUD"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'e9371d0c3b57e94ab25b6c4a7c3d7e2bbb95d344629b4e0047a6fd1370d7f619d54090dfb460e1f2967870b074806e2a0fe3efec194bffae25eec3994ecd3980'
